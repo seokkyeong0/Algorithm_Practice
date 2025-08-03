60f2bffc0509f2a754adc8fe1924cdfde0a31dc0
Nori <seokkyeong0@gmail.com>
2025-08-03 10:46:09 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+X = int(input())
+N = int(input())
+
+sum = 0
+for i in range(N):
+    A, B = map(int, input().split())
+    sum += (A*B)
+
+if sum == X:
+    print(f"Yes")
+else:
+    print(f"No")
