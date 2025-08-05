8981527999854aa105e067b99a1a78d6a3d44f5f
Nori <seokkyeong0@gmail.com>
2025-08-05 10:46:57 +0900

    [Silver IV] Title: 카드2, Time: 624 ms, Memory: 96920 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    def enqueue(self, data):
+        if self.front == None:
+            curr = Node(data)
+            self.front = curr
+            self.rear = curr
+        else:
+            curr = Node(data)
+            self.rear.next = curr
+            self.rear = curr
+    def dequeue(self):
+        if self.front == None:
+            return None
+        else:
+            curr = self.front
+            self.front = self.front.next
+            return curr.data
+    def is_empty(self):
+        return self.front == None
+    
+q = Queue()
+N = int(input())
+for i in range(1, N+1):
+    q.enqueue(i)
+
+while True:
+    if q.front == q.rear:
+        break
+    q.dequeue()
+    
+    if q.front == q.rear:
+        break
+    q.enqueue(q.dequeue())
+
+# q의 크기는 1
+print(q.dequeue())
