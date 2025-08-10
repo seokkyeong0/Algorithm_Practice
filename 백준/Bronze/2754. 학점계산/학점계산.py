4b0b188e2bae74bd3ab09e7fe07d43920c349e51
Nori <seokkyeong0@gmail.com>
2025-08-11 00:01:09 +0900

    [Bronze III] Title: 학점계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+S = input()
+
+if S == "A+":
+    print(f"{4.3}")
+if S == "A0":
+    print(f"{4.0}")
+if S == "A-":
+    print(f"{3.7}")
+if S == "B+":
+    print(f"{3.3}")
+if S == "B0":
+    print(f"{3.0}")
+if S == "B-":
+    print(f"{2.7}")
+if S == "C+":
+    print(f"{2.3}")
+if S == "C0":
+    print(f"{2.0}")
+if S == "C-":
+    print(f"{1.7}")
+if S == "D+":
+    print(f"{1.3}")
+if S == "D0":
+    print(f"{1.0}")
+if S == "D-":
+    print(f"{0.7}")
+if S == "F":
+    print(f"{0.0}")
