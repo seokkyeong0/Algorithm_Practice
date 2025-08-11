3b6e2cbaaf76acd11bb0ea449f52a27aadfff289
Nori <seokkyeong0@gmail.com>
2025-08-11 22:58:25 +0900

    [Silver V] Title: 단어 정렬, Time: 556 ms, Memory: 37532 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,10 @@
+N = int(input())
+words = set()
+
+for _ in range(N):
+    words.add(input())
+    
+sorted_words = sorted(words, key=lambda x: (len(x), x))
+
+for ch in sorted_words:
+    print(ch)
