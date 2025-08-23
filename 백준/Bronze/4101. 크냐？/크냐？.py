b9aa2fe7443229f5d99d5d5019f358d757655935
Nori <seokkyeong0@gmail.com>
2025-08-24 00:20:25 +0900

    [Bronze V] Title: 크냐?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,10 @@
+while True:
+    A, B = map(int, input().split())
+
+    if A == 0 and B == 0:
+        break
+    else:
+        if A > B:
+            print(f"Yes")
+        else:
+            print(f"No")
