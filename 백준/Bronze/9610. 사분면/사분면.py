02a1d3c7a57587d7ebeae5751eada92cb231b8ed
Nori <seokkyeong0@gmail.com>
2025-08-30 23:28:49 +0900

    [Bronze III] Title: 사분면, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9610. 사분면/사분면.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+axis = 0
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    if x > 0 and y > 0:
+        q1 += 1
+    elif x < 0 and y > 0:
+        q2 += 1
+    elif x < 0 and y < 0:
+        q3 += 1
+    elif x > 0 and y < 0:
+        q4 += 1
+    else:
+        axis += 1
+
+print(f"Q1: {q1}")
+print(f"Q2: {q2}")
+print(f"Q3: {q3}")
+print(f"Q4: {q4}")
+print(f"AXIS: {axis}")
