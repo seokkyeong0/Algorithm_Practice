07729240c4e0ee74e4ed64e6042a524e3d79fdd5
Nori <seokkyeong0@gmail.com>
2025-09-01 13:58:32 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+
+sequence = []
+
+def backtrack():
+    # 재귀 호출 후 조건을 만족하면 출력하고 return으로 종료
+    if len(sequence) == M:
+        print(*sequence)
+        return
+
+    for i in range(1, N + 1):
+        if len(sequence) == 0:
+            sequence.append(i)
+        else:
+            if i >= sequence[-1]:
+                sequence.append(i)
+            else:
+                continue
+
+        # 재귀 호출
+        backtrack()
+
+        # 호출 종료 후 pop
+        sequence.pop()
+backtrack()
