09c38c9155ffa8bd6ae5c008715b4597c90ffe4c
Kyeonghyun Seok <seokkyeong0@gmail.com>
2025-09-30 13:42:56 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	if (C - B <= 0) {
+		cout << -1;
+	}
+	else {
+		cout << A / (C - B) + 1;
+	}
+}
