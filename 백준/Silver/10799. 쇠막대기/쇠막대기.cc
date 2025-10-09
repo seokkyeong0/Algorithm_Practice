a9e170a18578b5ed325ff08c68739bc81241b9f2
Kyeonghyun Seok <seokkyeong0@gmail.com>
2025-10-09 19:43:51 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2692 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+
+int v[100000];
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+
+    int floor = 0, result = 0;
+
+    stack<char> st;
+    for (auto c : s) {
+        v[0] = 0;
+        if (st.empty()) {
+            st.push(c);
+            floor++;
+        }
+        else if (!st.empty() && st.top() == '(' && c == '(') {
+            st.push(c);
+            floor++;
+        }
+        else if (!st.empty() && st.top() == ')' && c == '(') {
+            st.push(c);
+            floor++;
+        }
+        else if (!st.empty() && st.top() == '(' && c == ')') {
+            st.push(c);
+            floor--;
+            v[floor] += 1;
+        }
+        else if (!st.empty() && st.top() == ')' && c == ')') {
+            st.push(c);
+            if (floor > 0) {
+                result += (v[floor] + 1);
+                v[floor - 1] += v[floor];
+                v[floor] = 0;
+                floor--;
+            }
+        }
+    }
+    cout << result;
+}
