5c7fdf6251fe2f647952899a2df448e0488c0c73
Kyeonghyun Seok <seokkyeong0@gmail.com>
2025-12-03 09:08:16 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	while (N--) {
+		string s;
+		cin >> s;
+
+		if (s == "Algorithm") cout << 204 << '\n';
+		else if (s == "DataAnalysis") cout << 207 << '\n';
+		else if (s == "ArtificialIntelligence") cout << 302 << '\n';
+		else if (s == "CyberSecurity") cout << "B101" << '\n';
+		else if (s == "Network") cout << 303 << '\n';
+		else if (s == "Startup") cout << 501 << '\n';
+		else if (s == "TestStrategy") cout << 105 << '\n';
+	}
+}
